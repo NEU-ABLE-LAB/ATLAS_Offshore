65df876708992963409b305ee948817f0e31561e
Michael Kane <mi.kane@northeastern.edu>
2019-04-19 13:51:19 +0000

    Fixed error in my_div where sign(0) is 0

--- OpenMLC-Matlab-2/MLC_tools/my_div.m
+++ OpenMLC-Matlab-2/MLC_tools/my_div.m
@@ -1,7 +1,7 @@
 function b=my_div(arg1,arg2)
 % copyright
     protec=0.001;
-    b=arg1./(arg2.*(abs(arg2)>=protec)+protec*sign(arg2).*(abs(arg2)<protec));
+    b=arg1./(arg2.*(abs(arg2)>=protec)+protec*sign(arg2+protec).*(abs(arg2)<protec));
 end
 
 
