9b49c163aa3ea2efc6f486502e7d8ac5bac2a9c5
JamesVelino <48775995+JamesVelino@users.noreply.github.com>
2021-02-01 23:21:13 -0500

    More post processing
    added summatry chart

--- MLC_Post.m
+++ MLC_Post.m
@@ -7,7 +7,7 @@ addpath(pwd)
 addpath(genpath([pwd,'/OpenMLC-Matlab-2'])); % OpenMLC classes and functions
 addpath(genpath([pwd,'/MLC_Problemfunctions'])); % functions related to the turbine problem
 addpath(genpath([pwd,'/ParforProgMon'])); % Parfor progress monitor
-    
+
 %% Load MLC object
 
 load('20201203_062004mlc_ae.mat')
@@ -15,6 +15,8 @@ load('20201203_062004mlc_ae.mat')
 [~,nGenerations] = size(mlc.population);
 [~,nInds] = size(mlc.population(1).individuals);
 
+
+%% Draw Plots
 DrawPlots = false;
 
 if DrawPlots
@@ -128,92 +130,131 @@ end
 
 
 
-%% Individuals to Retest on all SImulations
-%
+%% Individuals to Retest on all Simulations
+
 nReEval = 10; %re evaluare the top ## individuals in each generation
-nReEvalSets = 150; % break re evaluatibn into ## subsets, saving in between each subset
+
+nReEvalSets = 150; % break re evaluatibn into ## subsets, saving in between each subset, 
+%use to save information in event re evlauation process craches
 
 FastPath = 'D:\Documents\GitHub\ATLAS_FAST-par';
 MLCPath = 'D:\Documents\GitHub\ATLAS_Offshore';
 
-ReEvalFile = ['18-Jan-2021_MLCReEvaluation.mat'];
+ReEvalFile = ['22-Jan-2021_MLCReEvaluation'];
+ReEvalComplete = true;
 
-if   ~exist(ReEvalFile,'file')
-    %Prealocate
-    RE_IndNumberArray = zeros(nReEval, nGenerations);
+if ReEvalComplete ~= true
+    if   ~exist(ReEvalFile,'file')
+        %Prealocate
+        RE_IndNumberArray = zeros(nReEval, nGenerations);
         
-    %Create array of top inds, NReEval x NGenerations
-    TopInds = zeros(nReEval,1);
-    for Gen = 1 : nGenerations
-        TopInds = mlc.population(Gen).individuals(1:nReEval);
-        RE_IndNumberArray(:,Gen) = TopInds';   
-    end
-    
-    RE_IndNumbs = unique(RE_IndNumberArray);
-    RE_Tracker = zeros(length(RE_IndNumbs),1);
-    RE_CtrlEquation = cell(length(RE_IndNumbs),1);
-    RE_SimOut = RE_CtrlEquation; 
-    RE_CF = RE_CtrlEquation;
-    RE_Ind = RE_CtrlEquation;
-    
-    for ReEvalInd = 1 : length(RE_IndNumbs)
-        IndNumber = RE_IndNumbs(ReEvalInd);
-        ind = mlc.table.individuals(IndNumber);
-        RE_Ind{ReEvalInd} = ind;
-        [~,RE_CtrlEquation{ReEvalInd}] = MLC_MLC2Fast(ind.formal, mlc.parameters);
-    end
-    
-    FileName = strcat(date(),'_MLCReEvaluation');
-    save(FileName,'RE_CF', 'RE_CtrlEquation', 'RE_Ind', 'RE_IndNumberArray', 'RE_IndNumbs', 'RE_SimOut', 'RE_Tracker')
-else
-    load(ReEvalFile);
-    FileName = ReEvalFile;
-end
-
-for Set =  1: nReEvalSets
-    %get individuas in this set:
-    SetSize = ceil(length(RE_IndNumbs)/nReEvalSets);
-    SetSizeMax = min(SetSize,length(RE_IndNumbs));%To prevent it from indexing into empty individuals
-    
-    SetNumbers = (Set - 1)*SetSize + [1:SetSizeMax];
-    
-    %Check to see if individuals in this set have been re-evaluated
-    IsNotReEvaluated = RE_Tracker(SetNumbers) == 0;
-    SetNumbers = SetNumbers(IsNotReEvaluated);
-    SetSize = length(SetNumbers);
-    
-    
-    % Get controler text for this set
-    SetfcnText = RE_CtrlEquation(SetNumbers);
-    
-    
-    if ~isempty(SetfcnText)
-        %re evaluate the controlers on all load cases
-        [CF, SimOut, Costs] = MLC_PostEval(SetfcnText, mlc.parameters, FastPath, MLCPath);
-        
-        %Post Processing
-        RE_Tracker(SetNumbers) = 1;
-        for ind = 1:SetSize
-            Setind = SetNumbers(ind);
-            RE_CF{Setind} = CF(ind);
-            RE_SimOut{Setind} = SimOut(:,ind);
-            RE_Ind{Setind}.cost = CF(ind).CF;
-            for LoadCase = 1:12
-                RE_Ind{Setind}.cost_history(LoadCase) = Costs{LoadCase,ind};
-            end
+        %Create array of top inds, NReEval x NGenerations
+        TopInds = zeros(nReEval,1);
+        for Gen = 1 : nGenerations
+            TopInds = mlc.population(Gen).individuals(1:nReEval);
+            RE_IndNumberArray(:,Gen) = TopInds';
         end
         
-        save(FileName,'RE_CF', 'RE_CtrlEquation', 'RE_Ind', 'RE_IndNumberArray', 'RE_IndNumbs', 'RE_SimOut', 'RE_Tracker','-v7.3')
+        RE_IndNumbs = unique(RE_IndNumberArray);
+        RE_Tracker = zeros(length(RE_IndNumbs),1);
+        RE_CtrlEquation = cell(length(RE_IndNumbs),1);
+        RE_SimOut = RE_CtrlEquation;
+        RE_CF = RE_CtrlEquation;
+        RE_Ind = RE_CtrlEquation;
+        
+        for ReEvalInd = 1 : length(RE_IndNumbs)
+            IndNumber = RE_IndNumbs(ReEvalInd);
+            ind = mlc.table.individuals(IndNumber);
+            RE_Ind{ReEvalInd} = ind;
+            [~,RE_CtrlEquation{ReEvalInd}] = MLC_MLC2Fast(ind.formal, mlc.parameters);
+        end
+        
+        FileName = strcat(date(),'_MLCReEvaluation');
+        save(FileName,'RE_CF', 'RE_CtrlEquation', 'RE_Ind', 'RE_IndNumberArray', 'RE_IndNumbs', 'RE_SimOut', 'RE_Tracker')
+    else
+        load(ReEvalFile);
+        FileName = ReEvalFile;
     end
     
+    for Set =  1: nReEvalSets
+        %get individuas in this set:
+        SetSize = ceil(length(RE_IndNumbs)/nReEvalSets);
+        SetSizeMax = min(SetSize,length(RE_IndNumbs));%To prevent it from indexing into empty individuals
+        
+        SetNumbers = (Set - 1)*SetSize + [1:SetSizeMax];
+        
+        %Check to see if individuals in this set have been re-evaluated
+        IsNotReEvaluated = RE_Tracker(SetNumbers) == 0;
+        SetNumbers = SetNumbers(IsNotReEvaluated);
+        SetSize = length(SetNumbers);
+        
+        
+        % Get controler text for this set
+        SetfcnText = RE_CtrlEquation(SetNumbers);
+        
+        
+        if ~isempty(SetfcnText)
+            %re evaluate the controlers on all load cases
+            [CF, SimOut, Costs] = MLC_PostEval(SetfcnText, mlc.parameters, FastPath, MLCPath);
+            
+            %Post Processing
+            RE_Tracker(SetNumbers) = 1;
+            for ind = 1:SetSize
+                Setind = SetNumbers(ind);
+                RE_CF{Setind} = CF(ind);
+                RE_SimOut{Setind} = SimOut(:,ind);
+                RE_Ind{Setind}.cost = CF(ind).CF;
+                for LoadCase = 1:12
+                    RE_Ind{Setind}.cost_history(LoadCase) = Costs{LoadCase,ind};
+                end
+            end
+            
+            save(FileName,'RE_CF', 'RE_CtrlEquation', 'RE_Ind', 'RE_IndNumberArray', 'RE_IndNumbs', 'RE_SimOut', 'RE_Tracker','-v7.3')
+        end
+        
+    end
 end
 
+%% ReEevaluation analysis
 
+load(ReEvalFile);
 
+for ind = 1 : length(RE_CF)
+    Costs(ind) = RE_CF{ind}.CF;
+end
 
+Min_Cost = min(Costs);
+Min_IndNum = RE_IndNumbs(Costs == Min_Cost);
+MinIndLoc = find(RE_IndNumberArray == Min_IndNum) / nReEval;
+Min_IndGen = ceil(MinIndLoc);
+Min_IndGenRank = round((MinIndLoc - floor(MinIndLoc)) * nReEval,0);
+Min_CtrlLaw = RE_CtrlEquation{Costs == Min_Cost};
 
+%Print Best Inividual Properties
+fprintf('BEST INDIVIDUAL:\nIndividual number %i\nGeneration %i, Gen. Rank %i\nCOST = %f\n\n',Min_IndNum,Min_IndGen,Min_IndGenRank,Min_Cost)
 
+%% Plot histogram with overall cost trend of best individual in each gen.
 
+StartGen = 25; %Generation To start plot at 
+EndGen = 100; %Generation to end plot at
+
+figure
+hold on
+for Gen = 1 : EndGen - StartGen + 1
+    GenNum = StartGen + Gen - 1;
+    AllCosts(:,Gen) = mlc.population(Gen).costs; 
+    for TopInd = 1: nReEval   
+        TMinTotal(TopInd) = min(Costs(RE_IndNumbs == RE_IndNumberArray(TopInd,GenNum)));
+    end
+    MinTotal(Gen) = min(TMinTotal);
+    Gens(Gen) = GenNum;
+end    
+boxplot(AllCosts,Gens,'PlotStyle','compact','OutlierSize',1)
+plot(MinTotal,'LineWidth',4)
+ylim([.5,1.5])
+xlabel('Generation')
+ylabel('Cost')
+hold off
 
 
 
